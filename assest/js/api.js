173b00276476c5ad4e97a0af7ca6fe89bf3263b6
unknown <tanyalyt80@gmail.com>
2024-02-29 13:41:59 +0200

    added api, route.js, module.js, js header mob

---
+++ assest/js/api.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const apiKey = "c36508d37286c777e3389fe115f253ab";
+
+/**
+ * Fetch data from server
+ * @param {string}  URL API url
+ * @param {Function} callback callback
+ */
+export const fetchData = function (URL, callback) {
+  fetch(`${URL}&appid=${apiKey}`)
+    .then(res => res.json())
+    .then(data => callback(data));
+}
+
+export const url= {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+  },
+
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast??${lat}&${lon}&units=metric`;
+  },
+
+  airPollution(lat, lon) {
+    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+  },
+
+ reverseGeo(lat, lon) {
+   return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+ },
+
+
+ /**
+  * @param {string} query 
+  */
+  geo(query) {
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+  },
+}
+
+// http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5&appid=c36508d37286c777e3389fe115f253ab
+
+// http://api.openweathermap.org/geo/1.0/direct?q=Warsaw&limit=5&appid=c36508d37286c777e3389fe115f253ab
+
+// https://api.openweathermap.org/data/2.5/weather?lat=51.5072&lon=0.1276&units=metric&appid=c36508d37286c777e3389fe115f253ab
